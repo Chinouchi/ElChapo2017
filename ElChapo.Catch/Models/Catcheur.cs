ab4027f7a4a0c4d0039171f3eee799d68a4d115c
the_k <the_k@ANTOINE-SURFACE>
2017-04-06 16:40:53 +0200

    Add project files.

---
+++ ElChapo.Catch/Models/Catcheur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElChapo.Catch.Models
+{
+    enum EtatEnum
+    {
+        Disponible,
+        Convalescence,
+        Mort
+    }
+
+    class Catcheur
+    {
+        public String Name { get; set; }
+        public EtatEnum Etat { get; set; }
+    }
+}
---
+++ ElChapo.Catch/Program.cs
@@ -0,0 +1,27 @@
+﻿using ElChapo.Catch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElChapo.Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Catcheur> catcheurs = new List<Catcheur>();
+            
+            IEnumerable<Catcheur> disponibles = catcheurs.Where(c => c.Etat == EtatEnum.Disponible).OrderBy(c => c.Name);
+
+            Console.ReadLine();
+        }
+
+        static void MaFonction(Catcheur catcheur, int t)
+        {
+            catcheur.Name = "toto";
+            t = 10;
+        }
+    }
+}
